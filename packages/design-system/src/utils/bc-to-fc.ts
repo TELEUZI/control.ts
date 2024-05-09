c9e95e39d89dd50504adbfd572bddcba6435ada4
VKTRenokh <viktorenokh@gmail.com>
2024-05-09 13:11:08 +0300

    refactor: improve bc-to-fc jsdoc

--- packages/design-system/src/utils/bc-to-fc.ts
+++ packages/design-system/src/utils/bc-to-fc.ts
@@ -4,6 +4,22 @@ import type { BaseComponent } from '@control.ts/min';
  * Converts a class component to a functional component.
  * @param bc - The class component to convert.
  * @returns - A function that creates instances of the converted functional component.
+ *
+ * @example
+ *
+ * class SomeComponent extends BaseComponent {
+ *   constructor(public someArg: string) {
+ *     super({});
+ *   }
+
+ *   public someMethod(someArg: number) {
+ *     console.log("i'm useless", someArg);
+ *   }
+ * }
+
+ * const Some = bcToFc(SomeComponent);
+
+ * Some('hello').someMethod(50); // "i'm useless" 50
  */
 export const bcToFc =
   <T extends BaseComponent, A extends ReadonlyArray<unknown>>(bc: { new (...args: A): T }) =>
