2ebfead98d29b6a31bc00a1904267524fbc6ba97
Yauheni Tsurankou <ikk.pott@gmail.com>
2026-06-06 19:05:19 +0300

    refactor: implement single factory for components

--- packages/signals/src/factories.ts
+++ packages/signals/src/factories.ts
@@ -1,4 +1,4 @@
-import type { TagName } from '@control.ts/control';
+import { createElementFactoryFabric } from '@control.ts/control';
 
 import type { BaseComponentChild, BaseComponentProps } from './base-component';
 import { BaseComponent } from './base-component';
@@ -6,14 +6,8 @@ import { BaseComponent } from './base-component';
 export type { ElementFnProps, TagName } from '@control.ts/control';
 export { createElement, createElementFactory, createElementFactoryWithCustomProps } from '@control.ts/control';
 
-export function createElement$<T extends TagName>(
-  props: BaseComponentProps<HTMLElementTagNameMap[T]> & { tag: T },
-  children: BaseComponentChild[],
-) {
-  return new BaseComponent<HTMLElementTagNameMap[T]>(props, ...children);
-}
-
-export function createElementFactory$<T extends TagName>(tag: T) {
-  return (props: BaseComponentProps<HTMLElementTagNameMap[T]>, ...children: BaseComponentChild[]) =>
-    createElement$({ tag, ...props }, children);
-}
+export const createElementFactory$ = createElementFactoryFabric<
+  BaseComponentProps<HTMLElement>,
+  BaseComponentChild<HTMLElement>,
+  BaseComponent<HTMLElement>
+>(BaseComponent);
