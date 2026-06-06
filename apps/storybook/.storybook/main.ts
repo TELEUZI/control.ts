19b52637248c6ef1024b048846883237e009f67d
Yauheni Tsurankou <ikk.pott@gmail.com>
2026-06-06 18:15:32 +0300

    chore: update Storybook application with vite and base configuration

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,6 +1,9 @@
 import type { StorybookConfig } from '@storybook/html-vite';
 
 import { join, dirname } from 'path';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
